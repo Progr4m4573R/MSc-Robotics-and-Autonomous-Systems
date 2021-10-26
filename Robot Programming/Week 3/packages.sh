5718af84e26192ed845dcd44e497f73bbfcfa6ce
Computing <ghostye@hotmail.com>
2021-10-26 16:04:27 +0100

    Did the week 3 workshop

---
+++ Robot Programming/Week 3/packages.sh
@@ -0,0 +1,9 @@
+sudo apt-get install \
+    ros-melodic-opencv-apps \
+    ros-melodic-rqt-image-view \
+    ros-melodic-uol-cmp9767m-base \
+    ros-melodic-find-object-2d \
+    ros-melodic-video-stream-opencv \
+    ros-melodic-topic-tools \
+    ros-melodic-rqt-tf-tree
+
