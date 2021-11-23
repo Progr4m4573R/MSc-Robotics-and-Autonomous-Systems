d8f5b903aa579b342ce8ef95bf210ab258c2757f
Computing <ghostye@hotmail.com>
2021-11-23 15:35:44 +0000

    Started week 6 lecture, needs completing

---
+++ Robot Programming/Week 6/WorkShop/dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install \
+    ros-melodic-robot-localization \
+    ros-melodic-topological-navigation \
+    ros-melodic-amcl \
+    ros-melodic-fake-localization \
+    ros-melodic-carrot-planner
+
+echo 'Installing Week 7 dependencies for Localisation......'
