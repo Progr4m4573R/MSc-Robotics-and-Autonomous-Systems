b798fd61f582a9ce998f2cafb297bc1e9c46685a
Stephen <srerribekibele@gmail.com>
2021-11-28 09:44:50 +0000

    Added some link to my excell sheet that will be useful for the Advanced Artifiial Intelligence Assignment

---
+++ Foundations of Robotics/Foundations of robotics arduino codes/L293D/L293D.ino
@@ -0,0 +1,23 @@
+//https://www.youtube.com/watch?v=d7oFD-zQpuQ&ab_channel=MERTArduino%26Tech
+const int leftForward = 2;
+const int leftBackward = 3;
+const int rightForward = 4;
+const int rightBackward = 5;
+
+void setup() 
+{
+  pinMode(leftForward , OUTPUT);
+  pinMode(leftBackward , OUTPUT);
+  pinMode(rightForward , OUTPUT);
+  pinMode(rightBackward , OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(leftForward , HIGH);
+  digitalWrite(leftBackward , LOW);
+  digitalWrite(rightForward , HIGH);
+  digitalWrite(rightBackward , LOW);
+ 
+}
