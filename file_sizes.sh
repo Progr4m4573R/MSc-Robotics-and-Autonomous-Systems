39b1b895bfa5effd0b85ef944a54fe8d3128ba89
iGhostye <ghostye@hotmail.com>
2022-04-25 15:44:32 +0100

    Mofified my script for displaying file sizes

--- file_sizes.sh
+++ file_sizes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "source is: https://stackoverflow.com/questions/10622179/how-to-find-identify-large-commits-in-git-history"
+
 
  
 git rev-list --objects --all |
@@ -10,6 +10,7 @@ git rev-list --objects --all |
   cut -c 1-12,41- |
   $(command -v gnumfmt || echo numfmt) --field=2 --to=iec-i --suffix=B --padding=7 --round=nearest
 
+echo "source is: https://stackoverflow.com/questions/10622179/how-to-find-identify-large-commits-in-git-history"
 echo "Sizes of all files in git repo"
 
 exit
