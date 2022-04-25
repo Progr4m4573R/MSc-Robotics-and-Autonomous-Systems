4b605a14fdd4e9b9f8b2f9cfb6c8a1f0b4be4441
iGhostye <ghostye@hotmail.com>
2022-04-25 15:31:58 +0100

    Added some scripts to help check the size of the git repository

---
+++ repository_size.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Website with cool github commands https://stackoverflow.com/questions/10067848/remove-folder-and-its-contents-from-git-githubs-history
+
+
+echo "Checking size of repository..."
+git count-objects -vH
+
+exit
