03d519b009370bbad73d7d4df6d8e3bf763a8ef2
Harrison Leitch <harrisonleitch1@gmail.com>
2022-05-14 13:23:04 +0100

    Improved script for installing dependencies and training ai ot play mario and breakout

--- Advanced Machine Learning/autoinstall_dependencies.bat.sh
+++ Advanced Machine Learning/autoinstall_dependencies.bat.sh
@@ -9,7 +9,7 @@ pip install nes-py
 pip install gym-super-mario-bros
 pip install gym[accept-rom-license]
 pip install OpenCV-Python
-
+pip install filelock
 
 python train_dqn_ale.py --env BreakoutNoFrameskip-v4 --arch doubledqn --final-exploration-frames 300000 --steps 330000
 echo "The program has completed"
