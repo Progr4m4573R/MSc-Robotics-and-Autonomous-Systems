b86952226758a7d4a8fde7237f3de51f544c653b
unknown <ghostye@hotmail.com>
2022-05-16 14:57:37 +0100

    Finished Computer vision assignment task 2_1 fourier transform

--- Computer Vision/Assignment/CMP9135M_Assignment_1_task_2.m
+++ Computer Vision/Assignment/CMP9135M_Assignment_1_task_2.m
@@ -1,63 +1,11 @@
 clear; close all; clc
-%Task 2 Part 1 select features for radius and direction
+%------Task 2 Part 1 select features for radius and direction----------
 
 %https://uk.mathworks.com/help/images/create-image-histogram.html
 imgpia = imread('ImgPIA.jpg');
-figure
-imshow(imgpia)
-%set radius and angle
-radius = 100;%chosen radius
-angle = 360;%total number of angles
+Fourier_Analysis(imgpia)
 
-img = double(imgpia);
-[rows, columns] = size(imgpia);
-cy = round(rows/2);
-cx = round(columns/2);
-
-if exist('radius','var') == 0
-    radius = min(round(rows/2),round(columns/2))-1;
-end
-
-if exist('angle','var') == 0
-    angle = 360;
-end
-polarcord_img=zeros(); %from cartesian coordinate to polar
-i=1;
-%this loop performs the interpolation for radius and theata angle we use 0
-for r = 0:radius
-    j = 1;
-    for a=0:2*pi/angle:2*pi-2*pi/angle
-    polarcord_img(i,j) = img(cy+round(r*sin(a)),cx+round(r*cos(a)));
-    j = j+1;
-    end
-    i = i+1;
-end
-%after converting image
-%%Spectral feature calculation
-S_r_theta = fft(polarcord_img);
-
-%plot for r=1:100
-S_r=zeros();
-for r=1:100
-    S_r(r) = sum((abs(S_r_theta(r+1,:))));
-end
-figure(1)
-%in order to get spectral feature i convert to fourier domain
-bar(S_r)
-xlabel('radius')
-ylabel('feature value')
-%Plot for theta=0:360
-S_theta=zeros();
-for theta=1:360
-    S_theta(theta) = sum ((abs(S_r_theta(:,theta))));
-end
-
-figure(2)
-bar(S_theta)
-xlabel('theta')
-ylabel('feature value')
-
-%Task 2 Part 2 %co-currence matrix
+%-------------Task 2 Part 2 %co-currence matrix----------------------
 
 grayscale = rgb2gray(imgpia);
 %https://uk.mathworks.com/help/images/ref/imhist.html
--- Computer Vision/Assignment/Fourier_Analysis.m
+++ Computer Vision/Assignment/Fourier_Analysis.m
@@ -1,39 +1,59 @@
-%Prepare image
-%https://homepages.inf.ed.ac.uk/rbf/HIPR2/fourier.htm - helpful
-%understanding
-temp = imread('ImgPIA.jpg');
-grey = rgb2gray(temp);
-f = grey;
 
-figure;
-imshow(f);
-title('a 256 by 256 array of 0s')
-% Compute Fourier Transform
-F = fft2(f,256,256);
+%https://homepages.inf.ed.ac.uk/rbf/HIPR2/fourier.htm - helpful understanding
+%https://uk.mathworks.com/help/matlab/ref/fft2.html
+%https://uk.mathworks.com/help/matlab/ref/angle.html
+%code based on Computer vision workshop 8
+function[]=Fourier_Analysis(img)
+%Prepare image
+%-----------------------------------------------
+grey = rgb2gray(img);
+f = grey;
+%------------------------------------------------
+%rotate the image to 90 degree
+rotatedF = imrotate(f,90);
+%create the fourier transform of the image at 90 degrees
+%----------------------------------------------
+%return the two-dimensional Fourier transform of rotated image 
+F = fft2(rotatedF,256,256);
 figure;
 imshow(F);
-title('Fourier Transform')
-F = fftshift(F); % Center FFT
+title('Fast Fourier Transform at 90 degrees')
+%----------------------------------------------
+% rearranges the fourier transform by shifting the zero-frequency component to the center of the array.
+F = fftshift(F);
 figure;
 imshow(F);
-title('Centre the Fast Fourier Transform')
-% Measure the minimum and maximum value of the transform amplitude
-% min(min(abs(F)))
-% max(max(abs(F)))
-figure;
-imshow(abs(double(F)),[0 100]); colormap(jet); colorbar
-title('Amplitude')
+title('Centre of Fast Fourier Transform image at 90 degrees')
+%-----------------------------------------------
+figure
+imshow(abs(F),[0 100]); colormap(jet); colorbar
+title('Absolute value for the FFT of the image at 90 degrees')
+%----------------------------------------------
+%applying logarithm function to the fft image with offset
 figure;
 imshow(log(1+abs(double(F))),[2,3]); colormap(jet); colorbar
-title('Logarithm of Amplitude')
-% Look at the phases
+title('Logarithm for the fft of image with offset [2,3] at 90 degrees')
+% ---------------------------------------------------------
+% Phase angle of image with offset [2,3] and angle 90 degrees
 figure;
-imshow(angle(double(F)),[0,pi]); colormap(jet); colorbar
-title('Phases')
+imshow(angle(double(F)),[-pi,pi]); colormap(jet); colorbar
+title('Phase angle for ftt of image with offset [2,3] at 90 degrees')
+%---------------------------------------------------------
+%;Logarithm of fft image with offset [0,3]
+figure;
+imshow(log(1+abs(F)),[0,3]); colormap(jet); colorbar
+title('Logarithm for fft of image with offset [0,3] at 90 degrees')
+%---------------------------------------------------------
+%Phase angle of fft image with offset [2,3] at 90 degrees
+figure;
+imshow(angle(double(F)),[-pi,pi]);colorbar
+title('Phase angle for fft of image with offset [-pi,pi] at 90 degrees');
+%------------------------------------------------------------
+F = rgb2gray(img);
+F = imrotate(F,90);
+F = fft2(F,256,256);
 
-%inverse the Fourier Fast Transform
-I = ifft2(F, 256, 256);
 figure;
-imshow(I);
-title('Inverse of Fast Fourier Transform')
+imshow(ifft2(F,256,256));
+title('inversed fast fourier transform of FFT image at 90 degrees');
 
